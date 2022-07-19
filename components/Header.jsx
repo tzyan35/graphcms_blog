2048a1e23cf52f01524187669aa293a14bf19139
tzyan35 <zhongyan1997@hotmail.com>
2022-07-19 20:25:22 +0800

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         <div className="md:float-left block">
             <Link href="/">
                 <span className="cursor-pointer font-bold text-4xl text-white">
-                    GraphCMS
+                    GraphQL
                 </span>
             </Link>
         </div>
